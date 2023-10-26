8d1bffa5e5baac02f9e0d13a030f8bafebb05581
Olga <Olga@DESKTOP-HI8ST33>
2023-10-26 17:16:08 +0300

    Добавьте файлы проекта.

---
+++ RESRTfull.Domain/Data/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RESRTfull.Domain
+{
+    public class Address
+    {
+
+        public Guid ID { get; set; }
+        public string AddressPermanentRegistration { get; set; } = string.Empty;
+        public string? AddressTemporaryRegistration { get; set; }
+        //сделать через modelBuilder.Entity<BlogHeader>()
+        public Guid IndividualID { get; set; }
+        [ForeignKey("IndividualID")]
+        public Individual Individual { get; set; } = null!;
+    }
+}
